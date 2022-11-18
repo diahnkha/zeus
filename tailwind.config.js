f70c5dfe29d944cc4affd3193605adb695627253
diahnkha <diahnurkhasanah5@gmail.com>
2022-11-18 10:41:43 +0700

    add responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html"],
-    theme: {
-      extend: {},
+  content: ["./index.html","./node_modules/flowbite/**/*.js"],
+  theme: {
+    container: {
+        center: true,
+        screens: {
+            xl: '1170px'
+        },
     },
-    plugins: [],
-  }
+    fontFamily:{
+            'sans': ['Space Grotesk','Syne'],
+            'serif': ['Space Grotesk', 'Syne'],
+            'mono': ['Space Grotesk', 'Syne'],
+            'display': ['Space Grotesk', 'Syne'],
+            'body': ['Space Grotesk', 'Syne'],
+    },
+    extend: {
+      colors: {
+        myBlue: '#217BF4',
+      }
+    }
+  },
+  plugins: [require('flowbite/plugin')],
+}
+
+
